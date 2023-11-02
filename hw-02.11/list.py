fd34a3512d4c8a6f1c0656d1fff8b93192eff64e
KHNartay <149667695+KHNartay@users.noreply.github.com>
2023-11-02 16:24:46 +0600

    Create list.py
    home task

---
+++ hw-02.11/list.py
@@ -0,0 +1,147 @@
+#home task-list
+
+'''
+1
+fruits = []
+print(fruits)
+'''
+
+'''
+2
+fruits = ['orange', 'lemon', 'mango', 'banana', 'apple']
+print(fruits)
+'''
+
+'''
+3
+fruits = ['orange', 'lemon', 'mango', 'banana', 'apple']
+print(len(fruits))
+'''
+
+'''
+4
+fruits = ['orange', 'lemon', 'mango', 'banana', 'apple']
+a = fruits[0]
+b = fruits[2]
+c = fruits[-1]
+print(a, b, c)
+'''
+
+'''
+5
+mixed_data_types = ['Nartat', 13, 170, 'Mamyr 1']
+print(mixed_data_types)
+'''
+
+'''
+6
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(it_company)
+'''
+
+'''
+7
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(it_company)
+'''
+
+'''
+8
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(len(it_company))
+'''
+
+'''
+9
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+fruits = ['orange', 'lemon', 'mango', 'banana', 'apple']
+a = fruits[0]
+b = fruits[3]
+c = fruits[-1]
+print(a, b, c)
+'''
+
+'''
+10
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+it_company[0] = 'Samsung'
+print(it_company)
+'''
+
+'''
+11
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+num = [1, 2, 3, 4, 5,]
+it_company.extend(num)
+print(it_company)
+'''
+
+'''
+12
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+num = [1, 2, 3, 4, 5,]
+it_company.insert(4, num)
+print(it_company)
+'''
+
+'''
+16
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(sorted(it_company))
+'''
+
+'''
+17
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(verse(it_company))
+'''
+
+'''
+18
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(it_company[:3])
+'''
+
+'''
+19
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(it_company[-3:])
+'''
+
+'''
+20
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+print(it_company[3])
+'''
+
+'''
+21
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+it_company.remove('knniga')
+print(it_company)
+'''
+
+'''
+22
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+it_company.remove('Apple')
+print(it_company)
+'''
+
+'''
+23
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+it_company.remove('Amozon')
+print(it_company)
+'''
+
+'''
+24
+it_company = ['kniga', 'Google', 'Microosoft', 'Apple', 'IBM', 'Oracle', 'Amozon']
+it_company.clear
+print(it_company)
+'''
+
+
+
+#13, 14, 15, 25, 26, 27 --
